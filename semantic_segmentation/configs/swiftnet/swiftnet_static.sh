1e36aa88c4edeb474c3168db8615acaff6415054
Thomas Verelst <thomas.verelst@hotmail.com>
2021-11-17 16:43:33 +0100

    fixed swiftnet configs

--- semantic_segmentation/configs/swiftnet/swiftnet_static.sh
+++ semantic_segmentation/configs/swiftnet/swiftnet_static.sh
@@ -1,4 +1,4 @@
-python test.py --cityscapes-dir /esat/tiger/tverelst/dataset/cityscapes \
---network swiftnet_resnet50 --model-checkpoint pretrained/swiftnet_rn50.pth --half \
+python test_swiftnet.py --cityscapes-dir /esat/tiger/tverelst/dataset/cityscapes \
+--model-backbone resnet50 --model-checkpoint pretrained/swiftnet_rn50.pth --half \
 --block-policy stati --block-size 128 \
 --clip-length 20 --num-clips-warmup 500 --num-clips-eval -1
